74cadc62954db411bfa02b04c22b3c3f690d1082
ijonk7 <jookeo.rizal@gmail.com>
2021-02-22 23:58:33 +0700

    Update Email Verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new \App\Notifications\Auth\VerifyEmail);
+    }
 }
